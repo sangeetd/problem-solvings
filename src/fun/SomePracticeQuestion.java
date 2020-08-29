81a35e9cb7274f1a4407c0defbad79376ac36df5
sangeet das <imsangeet3@gmail.com>
2020-08-29 20:42:47 +0530

    matrix 90 deg anti rotate-update

--- src/fun/SomePracticeQuestion.java
+++ src/fun/SomePracticeQuestion.java
@@ -6225,15 +6225,15 @@ public class SomePracticeQuestion {
 //        M = 17;
 //        System.out.println(theMinimumCost_HackerEarth(N, M, 0));
 //..............................................................................
-        System.out.println("Inplace rotate square matrix by 90 degrees");
-        //https://www.geeksforgeeks.org/inplace-rotate-square-matrix-by-90-degrees/
-        int mat[][] = { 
-            { 1, 2, 3, 4 }, 
-            { 5, 6, 7, 8 }, 
-            { 9, 10, 11, 12 }, 
-            { 13, 14, 15, 16 } 
-        }; 
-        inPlace90AntiClockRotateMatrix(mat);       
+//        System.out.println("Inplace rotate square matrix by 90 degrees");
+//        //https://www.geeksforgeeks.org/inplace-rotate-square-matrix-by-90-degrees/
+//        int mat[][] = { 
+//            { 1, 2, 3, 4 }, 
+//            { 5, 6, 7, 8 }, 
+//            { 9, 10, 11, 12 }, 
+//            { 13, 14, 15, 16 } 
+//        }; 
+//        inPlace90AntiClockRotateMatrix(mat);       
 //..............................................................................
 //        System.out.println("longest common subsequence 3 ways");
 //        String a = "abcdefg";
