00b8b7a50ac086d44308d0dfb5250e308e479c95
sangeet das <imsangeet3@gmail.com>
2020-09-09 22:45:19 +0530

    ABB updaated to check final variables

--- src/fun/ABB.java
+++ src/fun/ABB.java
@@ -11,6 +11,19 @@ package fun;
  */
 public class ABB {
     
+    
+    final int var;
+    
+    static final int statVar = 1000;
+
+    public ABB() {
+        this.var = 10;
+    }
+    
+    public ABB(int var) {
+        this.var = var;
+    }
+    
     public int x;
 
     protected int y;
@@ -21,7 +34,21 @@ public class ABB {
 
         y =  b;
 
-    }        
+    }     
+    
+    public static void main(String[] args) {
+        
+        ABB ob1 = new ABB();
+        System.out.println(ob1.var);
+        ABB ob2 = new ABB();
+        System.out.println(ob2.var);
+        
+        ABB ob3 = new ABB(20);
+        System.out.println(ob3.var);
+        ABB ob4 = new ABB(50);
+        System.out.println(ob4.var);
+        
+    }
 
     
 }
